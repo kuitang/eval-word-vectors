de59fe468d758d4232d01c46029959306f1ef8a4
Kui Tang <kuitang@gmail.com>
2016-01-22 20:37:50 -0500

    Added open vocabulary analogy completion evaluation code

---
+++ model_all_analogy_open_vocab.py
@@ -0,0 +1,58 @@
+"""
+Model-based open vocabulary analogy completion.
+
+Use model.predict_analogy(a,b,a*) => Returns candidate b*.
+"""
+
+from read_analogy_open_vocab import analogy_readers
+from collections import Counter
+
+import pandas as pd
+
+def model_all_wordsim(model):
+    """Return DataFrame of analogy completion
+
+    model must the following methods:
+      model.predict_analogy(a, b, a*) for string words a, b, a* => Returns prediction candidate word b*.
+      model.__contains__(a) for string word a
+    """
+
+    rows = []
+    for dataset, reader in analogy_readers.iteritems():
+        counts = Counter()
+        correct_counts = Counter()
+
+        for row in reader():
+            counts[row.category] += 1
+
+            prediction = model.predict_analogy(*row.question)
+            if prediction.lower() == row.answer.lower():
+                correct_counts[row.category] += 1
+
+        # Collate statistics
+        for category, n_total in counts.iteritems():
+            accuracy = float(correct_counts[category]) / n_total
+            row = [dataset, category, accuracy]
+            rows.append(row)
+
+    return pd.DataFrame(rows, columns=['dataset', 'category', 'accuracy'])
+
+# Mock model for test case
+class ConstantModel(object):
+    """Return a constant prediction."""
+    def __init__(self, word):
+        """Set the word that we will return."""
+        self.word = word
+
+    def __contains__(self, w):
+        return True
+
+    def predict_analogy(self, a, b, a_star):
+        return self.word
+
+# Test case
+if __name__ == '__main__':
+    model = ConstantModel('sings')
+    results = model_all_wordsim(model)
+    print results
+
---
+++ read_analogy_open_vocab.py
@@ -0,0 +1,40 @@
+import os
+
+from collections import namedtuple
+from itertools import izip
+
+# If an analogy is a:b::a*:b*, the question is (a, b, a*) and the answer is b*.
+Question = namedtuple('Question', ['category', 'question', 'answer'])
+
+analogy_open_vocab_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                      'data',
+                                      'analogy-open-vocab')
+
+# Currently only handle the words; will do phrases later.
+def read_google():
+    fname = os.path.join(analogy_open_vocab_dir, 'google', 'questions-words.txt')
+    with open(fname, 'r') as of:
+        curr_category = None
+        for line in of:
+            # Are we at a category header?
+            toks = line.split()
+            if line[0] == ':':
+                curr_category = toks[1]
+            else:
+                assert curr_category is not None
+                yield Question(curr_category,
+                               tuple(toks[:3]),
+                               toks[3])
+
+def read_msr():
+    q_fname = os.path.join(analogy_open_vocab_dir, 'msr', 'word_relationship.questions')
+    a_fname = os.path.join(analogy_open_vocab_dir, 'msr', 'word_relationship.answers')
+
+    with open(q_fname, 'r') as qf, open(a_fname, 'r') as af:
+        for q_line, a_line in izip(qf, af):
+            category, answer = a_line.split()
+            yield Question(category,
+                           tuple(q_line.split()),
+                           answer)
+
+analogy_readers = {'google': read_google, 'msr': read_msr}
